a8b64255853a1f6d5e8ca002f7ad18744bf2594c
ashishnagar47 <nagar123ashish@gmail.com>
2020-06-03 11:57:57 +0530

    First Commit

--- src/public/app/navbar.js
+++
@@ -1,6 +0,0 @@
-let navlinks=$('.navbar-nav  .nav-link')
-
-navlinks.click((ev)=>{
-    let componentUrl=`/components/${$(ev.target).attr('data-component')}.html`
-    $('#contents').load(componentUrl)
-})
